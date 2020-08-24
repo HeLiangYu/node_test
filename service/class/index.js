c26475408f814246e54cc926ff48fdc80f494990
heliangyu <17826837202@163.com>
2020-08-24 23:30:51 +0800

    class

---
+++ service/class/index.js
@@ -0,0 +1,19 @@
+// const arr = [2, 3, 5];
+
+// for (let item of arr.values()) {
+//   console.log(item);
+// }
+
+class ts {
+  constructor(name) {
+    this.name = name;
+  }
+  static tst(name) {
+    // return new ts(name);
+    console.log(name);
+  }
+}
+
+const stts = new ts();
+ts.tst("k");
+// stts.tst("kl");
